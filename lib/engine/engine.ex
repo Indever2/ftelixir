df43bb086bf81068536dd8b02d0b4a7343188f60
Иван Кривцов <ivankrivcov@iMac-Ivan.cn.ru_novotelecom.ru>
2020-05-18 18:49:58 +0700

    Instant deletion. Index contains word's hash now.

--- lib/engine/engine.ex
+++ lib/engine/engine.ex
@@ -41,44 +41,24 @@ defmodule Ftelixir.Engine do
         update_index(state, word, {key, coff})
       end
     )
-    :ok = add_key_to_index(key, state.keys_table)
     {:reply, :ok, state}
   end
 
   def handle_call({:get, word}, _from, state) do
     case :ets.lookup(state.index_table, word) do
       [] -> {:reply, [], state}
-      [{^word, matches}] ->
-        existing_matches =
-          Enum.reduce(matches, [], fn {key, coff}, acc ->
-            if is_key_member?(key, state.keys_table) do
-              acc ++ [{key, coff}]
-            else
-              acc
-            end
-          end)
-
-        if matches != existing_matches do
-          if existing_matches != [] do
-            replace_index(state, word, existing_matches)
-          else
-            true = :ets.delete(state.index_table, word)
-            :ok = :dets.delete(state.disk_index_table, word)
-          end
-        end
-
-        {:reply, [{word, existing_matches}], state}
+      [{^word, _hash, matches}] -> {:reply, [{word, matches}], state}
     end
   end
 
   def handle_call(:all, _from, state) do
-    res = :ets.match_object(state.index_table, {:"$1", :"$2"})
+    res = :ets.match_object(state.index_table, {:"$1", :"$2", :"$3"})
     {:reply, res, state}
   end
 
   def handle_call({:delete, key}, _from, state) do
-    delete_key_from_index(key, state.keys_table)
-    {:reply, :ok, state}
+    res = delete_key_from_index(key, state)
+    {:reply, res, state}
   end
 
   def handle_call(:purge, _from, state) do
@@ -103,20 +83,27 @@ defmodule Ftelixir.Engine do
     {:stop, :normal}
   end
 
-  defp update_index(%{index_table: index_table, disk_index_table: disk_table}, word, value) do
-    case :ets.lookup(index_table, word) do
-      [] ->
-        true = :ets.insert(index_table, {word, [value]})
-        :ok = :dets.insert(disk_table, {word, [value]})
-      [{^word, index_list}] ->
-        true = :ets.insert(index_table, {word, index_list ++ [value]})
-        :ok = :dets.insert(disk_table, {word, index_list ++ [value]})
-    end
+  defp update_index(%{index_table: index_table, disk_index_table: disk_table, keys_table: keys_table}, word, {key, _data} = value) do
+    hash =
+      case :ets.lookup(index_table, word) do
+        [] ->
+          hash = :crypto.hash(:sha256, word)
+          true = :ets.insert(index_table, {word, hash, [value]})
+          :ok = :dets.insert(disk_table, {word, hash, [value]})
+          hash
+        [{^word, hash, index_list}] ->
+          true = :ets.insert(index_table, {word, hash, index_list ++ [value]})
+          :ok = :dets.insert(disk_table, {word, hash, index_list ++ [value]})
+          hash
+      end
+    :ok = store_key_and_hash(key, hash, keys_table)
   end
 
   defp replace_index(%{index_table: index_table, disk_index_table: disk_table}, word, value) do
-    true = :ets.insert(index_table, {word, value})
-    :ok = :dets.insert(disk_table, {word, value})
+    hash = :crypto.hash(:sha256, word)
+
+    true = :ets.insert(index_table, {word, hash, value})
+    :ok = :dets.insert(disk_table, {word, hash, value})
   end
   def add_record(index_name, key, index_list) do
     GenServer.call(index_name, {:add, key, index_list})
@@ -131,6 +118,10 @@ defmodule Ftelixir.Engine do
     GenServer.call(index_name, {:get, upcased})
   end
 
+  def lookup_hash(index_name, hash) do
+    GenServer.call(index_name, {:get, :hash, hash})
+  end
+
   def all_records(index_name) do
     GenServer.call(index_name, :all)
   end
@@ -139,19 +130,36 @@ defmodule Ftelixir.Engine do
     GenServer.call(index_name, :count)
   end
 
-  defp add_key_to_index(key, keys_table) do
-    :dets.insert(keys_table, {key})
-  end
-
-  defp is_key_member?(key, keys_table) do
+  defp store_key_and_hash(key, hash, keys_table) do
     case :dets.lookup(keys_table, key) do
-      [] -> false
-      [{^key}] -> true
+      [] ->
+        :dets.insert(keys_table, {key, [hash]})
+      [{key, hashes}] ->
+        :dets.insert(keys_table, {key, hashes ++ [hash]})
     end
   end
 
-  defp delete_key_from_index(key, keys_table) do
-    :dets.delete(keys_table, key)
+  defp delete_key_from_index(key, %{keys_table: keys_table, index_table: index_table, disk_index_table: disk_table} = tables) do
+    res = case :dets.lookup(keys_table, key) do
+      [] -> {:ok, :nothing_to_do}
+      [{^key, hashes}] ->
+        for hash <- hashes do
+          case :ets.match_object(index_table, {:"$1", hash, :"$3"}) do # TODO: func get_from_index_by_hash()
+            [] -> :ok # maybe shold be replaced
+            [{word, ^hash, references}] ->
+              case Enum.filter(references, fn {entry_key, _data} -> entry_key != key end) do
+                [] ->
+                  true = :ets.delete(index_table, word)
+                  :ok = :dets.delete(disk_table, word)
+                updated ->
+                  replace_index(tables, word, updated)
+              end
+          end
+        end
+        {:ok, :deleted}
+    end
+    :ok = :dets.delete(keys_table, key)
+    res
   end
 
   def purge(index_name) do
